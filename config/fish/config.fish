28b9ffe591390d3ac2c8d6b0f811d0e9138ae670
Sasha Friedenberg <sashafriedenberg@gmail.com>
2015-09-29 20:12:54 +0000

    add support for fish local config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,9 @@ if source rbenv > /dev/null ^ /dev/null
   . (rbenv init -|psub)
 end
 set fish_greeting
+
+set -l local_config ~/.config/fish/config.fish.local
+
+if test -e local_config
+  source local_config
+end
