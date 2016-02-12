f7c9e2d21c5013265dfce1f4e94b0c8daddd86de
Sasha Friedenberg <sasha@etsy.com>
2016-02-12 19:32:03 +0000

    add thefuck alias and add comments and use different command test

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,25 @@
-if source rbenv > /dev/null ^ /dev/null
-  . (rbenv init -|psub)
+# starting up rbenv
+if command -s rbenv > /dev/null
+  source (rbenv init -|psub)
 end
+
 set fish_greeting
 
+# aliasing `thefuck` to `fuck`
+if command -s thefuck > /dev/null
+  eval (thefuck --alias | tr '\n' ';')
+end
+
+# hushing motd unless it has changed
+cmp -s ~/.hushlogin /etc/motd
+if test $status -ne 0
+  tee ~/.hushlogin < /etc/motd
+end
+
+# loading local fish config, if it exists
 set -l local_config ~/.config/fish/config.fish.local
 
 if test -e local_config
   source local_config
 end
 
-cmp -s ~/.hushlogin /etc/motd
-if test $status -ne 0
-  tee ~/.hushlogin < /etc/motd
-end
