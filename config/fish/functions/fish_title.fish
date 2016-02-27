f83bef4697ac8fc9d870ff9b4210143e35483691
Sasha Friedenberg <sasha@etsy.com>
2016-02-27 21:50:09 +0000

    add support for outside of tmux terminal titles

--- config/fish/functions/fish_title.fish
+++ config/fish/functions/fish_title.fish
@@ -1,6 +1,8 @@
 function fish_title
   if test -n "$TMUX" -a -n "$ssh_config_current_alias"
     echo $ssh_config_current_alias
+  else if test -z "$TMUX" -a -n "$ssh_config_current_alias"
+    echo "$ssh_config_current_alias: $_"
   else
     echo $_
   end
