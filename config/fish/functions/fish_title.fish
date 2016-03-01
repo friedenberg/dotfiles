b1064f751e509ac3e0a5cf86d4239a66cd31b99b
Sasha Friedenberg <sasha@etsy.com>
2016-03-01 12:04:27 -0500

    [fish_title] add working directory when outside of tmux

--- config/fish/functions/fish_title.fish
+++ config/fish/functions/fish_title.fish
@@ -2,7 +2,7 @@ function fish_title
   if test -n "$TMUX" -a -n "$ssh_config_current_alias"
     echo $ssh_config_current_alias
   else if test -z "$TMUX" -a -n "$ssh_config_current_alias"
-    echo "$ssh_config_current_alias: $_"
+    echo "$ssh_config_current_alias: " (prompt_pwd)
   else
     echo $_
   end
