90b90fbad726c1736206457c69ba18d45a0e5019
Sasha Friedenberg <sasha@etsy.com>
2017-02-10 13:53:06 -0500

    [fish][greeting] use figlet if it's available

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,7 +1,13 @@
 function fish_greeting --description 'Write out the greeting'
-  set -l FISH_ASCII_PATH ~/.config/fish/fish.txt
+  if command -s figlet > /dev/null
+    set fonts (find (figlet -I2) -name '*.flf')
+    set i (math 'scale=0;'(random)'%'(count $fonts)'+1')
+    echo fish | figlet -f $fonts[$i]
+  else
+    set FISH_ASCII_PATH ~/.config/fish/fish.txt
 
-  if test -f "$FISH_ASCII_PATH"
-    cat "$FISH_ASCII_PATH"
+    if test -f "$FISH_ASCII_PATH" -a 0 -eq 1
+      cat "$FISH_ASCII_PATH"
+    end
   end
 end
