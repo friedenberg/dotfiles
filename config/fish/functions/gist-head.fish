32adb2f6886e4c47306176fe9caaa18269fdb13c
Sasha Friedenberg <sasha@etsy.com>
2017-02-10 17:47:18 +0000

    [fish][essence] add essence command for generating diff gists

--- config/fish/functions/gist-head.fish
+++
@@ -1,6 +0,0 @@
-function gist-head
-  set repo_name (basename (git rev-parse --show-toplevel))
-  set head_commit (git show --oneline --no-patch HEAD)
-  set gist_description "$repo_name: $head_commit"
-  git show HEAD | gist -t diff -d "$gist_description"
-end
