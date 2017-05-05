25d3b9579a98e574adfbe8666b9fbe5a7346a804
Sasha Friedenberg <sasha@etsy.com>
2017-05-05 11:23:48 -0400

    [fish_config] fix greeting

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -2,7 +2,7 @@ function fish_greeting --description 'Write out the greeting'
   if command -s figlet > /dev/null
     set fonts (find (figlet -I2) -name '*.flf')
     set i (math 'scale=0;'(random)'%'(count $fonts)'+1')
-    echo fish | figlet -f $fonts[$i]
+    echo fish | figlet -W -f $fonts[$i]
   else
     set FISH_ASCII_PATH ~/.config/fish/fish.txt
 
