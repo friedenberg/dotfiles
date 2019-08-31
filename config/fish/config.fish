d00b09ca6dbf9905f75096c4da7c2b72c5a45c60
Sasha Friedenberg <sashafriedenberg@gmail.com>
2019-08-31 11:58:50 -0400

    [fish] add minikube docker eval conditional

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -16,6 +16,14 @@ if command -s gpg-connect-agent > /dev/null ^ /dev/null
   set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 end
 
+if command -s minikube > /dev/null ^ /dev/null
+  set docker_eval (minikube docker-env --shell fish ^ /dev/null)
+
+  if test 0 -eq $status
+    eval $docker_eval
+  end
+end
+
 # aliasing `thefuck` to `fuck`
 if command -s thefuck > /dev/null
   eval (thefuck --alias | tr '\n' ';')
