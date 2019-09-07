26405dc896e09fea65d68412c26923000bc0379e
Sasha Friedenberg <sashafriedenberg@gmail.com>
2019-09-07 12:37:48 -0400

    [fish] update __complete_from function to support optional flags

--- config/fish/functions/__complete_from.fish
+++ config/fish/functions/__complete_from.fish
@@ -1,11 +1,17 @@
 function __complete_from --description "helper function to generate commandline completions"
   set last_char (commandline | string sub --start -1)
 
+  # we strip the command name fro the commandline
   set current_command_line (commandline | string trim | string split " ")[2..-1]
 
+  # if the last char isn't a space, we strip the last incomplete word from the
+  # commandline
   if not test "$last_char" = " "
     set current_command_line (string split -- " " $current_command_line)[1..-2]
   end
+
+  # strip flags out of the commandline
+  set current_command_line (echo $current_command_line | string replace --regex -- " -+\w+" "" | string split " ")
 
   set command_path $argv
 
