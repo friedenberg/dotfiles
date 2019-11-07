6ba6de5930b9535202d675b9768402b870c34db5
Sasha Friedenberg <sashafriedenberg@gmail.com>
2019-11-07 07:06:39 -0500

    update kubectl completions

--- config/fish/completions/kubectl.fish
+++ config/fish/completions/kubectl.fish
@@ -9,7 +9,11 @@ end
 __kubectl_c "(__complete_with config set-cluster __kubectl_print_clusters)"
 __kubectl_c "(__complete_with config use-context __kubectl_print_contexts)"
 __kubectl_c "(__complete_with logs __kubectl_print_pods)"
+__kubectl_c "(__complete_with exec -it __kubectl_print_pods)"
 __kubectl_c "(__complete_with describe __kubectl_print_pods)"
+__kubectl_c "(__complete_with delete pod __kubectl_print_pods)"
+__kubectl_c "(__complete_with delete deployment __kubectl_print_deployments)"
+__kubectl_c "(__complete_with edit deployment __kubectl_print_deployments)"
 __kubectl_c "(__complete_with apply -f __kubectl_print_yaml_configs)"
 
 function __kubectl_print_clusters
@@ -20,11 +24,15 @@ function __kubectl_print_contexts
   kubectl config get-contexts -o name
 end
 
+function __kubectl_print_deployments
+  kubectl get deployments | strip-first-line | spaces-to-tabs | cut -f1
+end
+
 function __kubectl_print_pods
   if test -n "$argv[1]"
-    kubectl get pods -n "$argv[1]" -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace | strip-first-line | tr " " "\t" | tr -s "\t" | cut -f1,2
+    kubectl get pods -n "$argv[1]" -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace | strip-first-line | spaces-to-tabs | cut -f1,2
   else
-    kubectl get pods --all-namespaces -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace | strip-first-line | tr " " "\t" | tr -s "\t" | cut -f1,2
+    kubectl get pods --all-namespaces -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace | strip-first-line | spaces-to-tabs | cut -f1,2
   end
 end
 
