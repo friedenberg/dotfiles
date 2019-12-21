bd6ee75922cbb749c4082870ce609c4cd3850800
Sasha Friedenberg <sashafriedenberg@gmail.com>
2019-12-21 12:02:27 -0500

    update ssh-get-pub-key script to include ASCII art and sorted output

--- config/fish/functions/ssh-get-pub-key.fish
+++ config/fish/functions/ssh-get-pub-key.fish
@@ -7,13 +7,19 @@ function ssh-get-pub-key
 
     case 1
       set -l pubkey (ssh-keyscan -t rsa $argv 2>&1)
-      set -l fingerprint (ssh-keygen -lf (echo $pubkey[2] | psub) 2>&1)
+      set -l fingerprint (ssh-keygen -v -lf (echo $pubkey[2] | psub) 2>&1)
       set -l lines (string split \n $pubkey)
-      set lines[1] (echo $lines[1] $fingerprint)
-      string join \n $lines
+
+      echo $pubkey[1]
+
+      for i in (seq (math (count $fingerprint) - 0))
+        echo \# $fingerprint[$i]
+      end
+
+      echo $pubkey[2]
 
     case '*'
-      for domain in $argv
+      for domain in (string split \n $argv | sort)
         ssh-get-pub-key $domain
       end
   end
