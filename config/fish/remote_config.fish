b3ea45c7ef003928be1fa6de7d10394230dd9446
Sasha Friedenberg <sashafriedenberg@gmail.com>
2020-06-24 11:22:41 -0400

    [fish] add helpful info for when ssh sessions don't have an alias

--- config/fish/remote_config.fish
+++ config/fish/remote_config.fish
@@ -1,3 +1,7 @@
 
 set -e SSH_AUTH_SOCK
 set -Ux SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock.$HOSTNAME
+
+if test -z "$ssh_config_current_alias"
+  echo 'No $ssh_config_current_alias is set for this ssh session.'
+end
