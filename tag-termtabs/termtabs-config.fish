81b4b596e8cb1aa16bfdcfbbef6f66254366e3ca
Sasha Friedenberg <sashafriedenberg@gmail.com>
2020-06-23 22:42:56 -0400

    [termtabs][fish] add missing condition for nested tmux session

--- tag-termtabs/termtabs-config.fish
+++ tag-termtabs/termtabs-config.fish
@@ -1,7 +1,9 @@
 # vim: set syntax=fish:
-#
-if tt has-session -t termtab > /dev/null ^ /dev/null
-  tt attach -t termtabs
-else
-  tt new-session -s termtabs
+
+if test -z "$TMUX"
+  if tt has-session -t termtab > /dev/null ^ /dev/null
+    tt attach -t termtabs
+  else
+    tt new-session -s termtabs
+  end
 end
