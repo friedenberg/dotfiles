a20f435af1c9729b562f819b3dcb8d9ba3b93594
Sasha Friedenberg <sashafriedenberg@gmail.com>
2020-06-25 17:07:25 -0400

    [fish][fish_title] use command title when in tmux and ssh

--- config/fish/functions/fish_title.fish
+++ config/fish/functions/fish_title.fish
@@ -1,5 +1,7 @@
 function fish_title
-  if test -n "$TMUX" -a -n "$ssh_config_current_alias"
+  if test -n "$SSH_CLIENT" -a -n "$TMUX"
+    echo $_
+  else if test -n "$TMUX" -a -n "$ssh_config_current_alias"
     echo $ssh_config_current_alias
   else if test -z "$TMUX" -a -n "$ssh_config_current_alias"
     echo "$ssh_config_current_alias: " (prompt_pwd)
