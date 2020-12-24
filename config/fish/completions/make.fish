7f6ccfe83d1c88d566bb20e12c87996abfd66ef5
Sasha Friedenberg <sashafriedenberg@gmail.com>
2020-12-24 10:35:47 -0500

    add make completion for targets

---
+++ config/fish/completions/make.fish
@@ -0,0 +1,19 @@
+complete -c make -f -a "(__make_complete_targets)"
+
+function __make_complete_targets
+  make -rpn \
+  | awk -F: '
+/# Files/ {
+    searching = 1
+  }
+
+/^[^#.][^%]+:/ {
+    if (searching && previous_line != "# Not a target")
+      print $1
+  }
+
+/^#/ {
+    previous_line = $1
+  }
+  '
+end
