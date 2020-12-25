ac00a447339b9eb2cabcf344a856d82917a43c50
Sasha Friedenberg <sashafriedenberg@gmail.com>
2020-12-25 12:09:06 -0500

    update reset-gpg

--- config/fish/functions/reset-gpg.fish
+++ config/fish/functions/reset-gpg.fish
@@ -3,6 +3,5 @@
 function reset-gpg --description "Restarts GPG and tests access to GitHub"
   gpgconf --kill gpg-agent; \
     and gpg-connect-agent /bye; \
-    and ssh-add -l; \
-    and ssh -T git@github.com
+    and string match -r "Hi \S+! You've successfully authenticated, but GitHub does not provide shell access." (ssh -T git@github.com 2>&1)
 end
