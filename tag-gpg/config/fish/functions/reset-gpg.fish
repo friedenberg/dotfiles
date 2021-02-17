3ad617e79610c584d6c92da2618bbb6b500ae945
Sasha Friedenberg <sashafriedenberg@gmail.com>
2021-02-17 08:55:36 -0500

    update reset-gpg

--- tag-gpg/config/fish/functions/reset-gpg.fish
+++ tag-gpg/config/fish/functions/reset-gpg.fish
@@ -1,6 +1,10 @@
 # vim: set syntax=fish:
 
 function reset-gpg --description "Restarts GPG and tests access to GitHub"
-  gpgconf --reload gpg-agent >/dev/null 2>&1 \
-    and gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
+  gpg-connect-agent "scd serialno" "learn --force" /bye \
+    and gpgconf --reload gpg-agent >/dev/null 2>&1 \
+    and gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1 \
+    and gpg --card-status >/dev/null 2>&1
+
+  return $status
 end
