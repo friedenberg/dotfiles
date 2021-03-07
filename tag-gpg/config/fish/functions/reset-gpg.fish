51f2209925415a5e06b0d791dd42529e11160efd
Sasha Friedenberg <sashafriedenberg@gmail.com>
2021-03-07 08:27:41 -0500

    run reset-gpg on all TTY commands

--- tag-gpg/config/fish/functions/reset-gpg.fish
+++ tag-gpg/config/fish/functions/reset-gpg.fish
@@ -1,6 +1,6 @@
 # vim: set syntax=fish:
 
-function reset-gpg --description "Restarts GPG and tests access to GitHub"
+function reset-gpg --on-event fish_preexec --description "Reloads GPG Agent"
   gpg-connect-agent "scd serialno" "learn --force" /bye \
     and gpgconf --reload gpg-agent >/dev/null 2>&1 \
     and gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1 \
