367d0f29ab58aa8b95b225b9dec6f8a55ac0db5a
Sasha Friedenberg <sashafriedenberg@gmail.com>
2021-05-24 06:48:28 -0400

    modify spaces-to-tabs to use builtin macos sed

--- config/fish/functions/spaces-to-tabs.fish
+++ config/fish/functions/spaces-to-tabs.fish
@@ -1,3 +1,3 @@
-function spaces-to-tabs --description "helper function that uses gsed to separate 2 or more spaces into tabs"
-  gsed -E 's/\s{2,}/\t/g'
+function spaces-to-tabs --description "helper function that uses sed to separate 2 or more spaces into tabs"
+  sed 's/[[:space:]][[:space:]][[:space:]]*/'\t'/g'
 end
