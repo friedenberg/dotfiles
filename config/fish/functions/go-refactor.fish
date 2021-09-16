b0e2a9649e4a469127087bbb8660b994a23d6c7e
Sasha Friedenberg <181492+friedenberg@users.noreply.github.com>
2021-09-15 18:35:52 -0700

    add gorefactor function

--- config/fish/functions/go-refactor.fish
+++ config/fish/functions/go-refactor.fish
@@ -1,11 +1,27 @@
 function go-refactor
-  set -l rewrite $argv[1]
-  set -l files $argv[2..-1]
-  gofmt -r $rewrite -d $files &| less
+  if test (count $argv) -lt 3
+    echo "Not enough arguments" >&2
+    return 1
+  end
 
-  echo "Proceed with refactor? (Y/n):" >&2
-  if test (string lower (read -n 1 -l)) = y
-    gofmt -r $rewrite -w $files
+  set -l pattern $argv[1]
+  set -l replacement $argv[2]
+  set -l files $argv[3..-1]
+  set -l base_gofmt_cmd gofmt -r "$pattern -> $replacement"
+  set -l file_diff (mktemp)
+  $base_gofmt_cmd -d $files > $file_diff
+
+  if test (wc -l < $file_diff) -eq 0
+    echo "Refactor produces no changes" >&2
+    return 1
+  else
+    less "$file_diff"
+  end
+
+  set -l prompt_cmd "Proceed with refactor? (Y/n):" >&2
+  set -l answer (read -n 1 -l -P $prompt_cmd)
+  if test (string lower $answer) = y
+    $base_gofmt_cmd -w $files
   end
 end
 
