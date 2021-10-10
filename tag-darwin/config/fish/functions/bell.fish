bd35ae098b4e0283dd37e3037e0274974cc24365
Sasha Friedenberg <181492+friedenberg@users.noreply.github.com>
2021-10-10 16:31:47 -0700

    add darwin-specific fish bell function

---
+++ tag-darwin/config/fish/functions/bell.fish
@@ -0,0 +1,11 @@
+function bell
+  set -l sound glass
+
+  if test (count $argv) -gt 0
+    set sound $argv[1]
+  end
+
+  set sound (string sub -l1 (string upper $sound))(string sub -s2 $sound)
+
+  afplay /System/Library/Sounds/$sound.aiff
+end
