5ae5d417179927e254aa0d1819a85f6ff6871c56
Sasha Friedenberg <181492+friedenberg@users.noreply.github.com>
2021-10-10 23:59:09 -0700

    add flags to mr-build-and-watch on fish/darwin

--- tag-darwin/config/fish/functions/mr-build-and-watch.fish
+++ tag-darwin/config/fish/functions/mr-build-and-watch.fish
@@ -1,8 +1,16 @@
 
 function mr-build-and-watch
-  set -l exclude $argv[1]
-  set -l command $argv[2]
+  argparse --name=mr-build-and-watch 'e/exclude=+' -- $argv
+  or return
 
-  fswatch -ro --exclude $exclude . \
+  set -l excludes
+
+  for f in $_flag_e
+    set -a excludes --exclude $f
+  end
+
+  set -l command $argv
+
+  fswatch -ro $excludes . \
   | xargs -I_ fish -c "echo; and $command; and echo done | ts; and bell; or bell Sosumi"
 end
