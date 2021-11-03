b562d40b66ffd59d78557ce0ef390fa1b0b03336
Sasha Friedenberg <181492+friedenberg@users.noreply.github.com>
2021-11-02 18:41:23 -0700

    add support for custom watch paths in mr-build-and-watch

--- tag-darwin/config/fish/functions/mr-build-and-watch.fish
+++ tag-darwin/config/fish/functions/mr-build-and-watch.fish
@@ -1,6 +1,9 @@
 
 function mr-build-and-watch
-  argparse --name=mr-build-and-watch 'e/exclude=+' -- $argv
+  argparse --name=mr-build-and-watch \
+    'e/exclude=+' \
+    'p/path=+' \
+    -- $argv
   or return
 
   set -l excludes
@@ -9,8 +12,14 @@ function mr-build-and-watch
     set -a excludes --exclude $f
   end
 
+  set -l watch_path (pwd)
+
+  if test (count $_flag_p) -gt 0
+    set watch_path $_flag_p
+  end
+
   set -l command $argv
 
-  fswatch -ro $excludes . \
+  fswatch -ro $excludes $watch_path \
   | xargs -I{} fish -c "echo; and $command; and echo done | ts; and bell; or bell Sosumi"
 end
