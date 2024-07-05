77ddb5bfc8d9e797f66efb9500d283bebc0646f9
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-07-05 09:06:17 -0400

    update

---
+++ config/nvim/lua/plugins/main.lua
@@ -0,0 +1,33 @@
+return {
+  {
+  "https://github.com/neovim/nvim-lspconfig/",
+  lazy = false,
+},
+{
+"altercation/vim-colors-solarized",
+lazy = false,
+},
+{
+"tpope/vim-commentary",
+lazy = false,
+},
+-- junegunn/vim-easy-align
+-- tpope/vim-abolish
+-- tpope/vim-projectionist
+-- tpope/vim-sensible
+-- LnL7/vim-nix
+-- StanAngeloff/php.vim
+-- aklt/plantuml-syntax
+-- bfrg/vim-jq
+-- cespare/vim-toml
+-- dag/vim-fish
+-- editorconfig/editorconfig-vim
+-- friedenberg/vim-fancy-comment
+-- friedenberg/vim-kitty-conf
+-- gisraptor/vim-lilypond-integrator
+-- ocaml/vim-ocaml
+-- osamuaoki/vim-spell-under
+-- stephpy/vim-php-cs-fixer
+-- tobyS/pdv
+-- vim-pandoc/vim-pandoc-syntax
+}
