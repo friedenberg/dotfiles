2222e50528241cd135d40a285ec2b6ef4d3f2ae9
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-07-26 08:34:25 -0400

    update

--- config/fish/functions/git-lazy-commit-and-push.fish
+++ config/fish/functions/git-lazy-commit-and-push.fish
@@ -1,3 +1,11 @@
 function git-lazy-commit-and-push
-  git commit -m update; and git push
+  set -l diff_status (git diff --cached 2>&1)
+
+  if test -n "$diff_status"; and not git commit -m update
+    return 1
+  else
+    echo "no changes, just pushing" >&2
+  end
+
+  git push
 end
