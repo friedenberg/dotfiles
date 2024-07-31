063568ad34fe1788641f924e8b70557fb0cc9d13
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-07-31 10:58:31 -0400

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #! /bin/bash -e
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 env
+pwd
 # TODO bootstrap nix
 # build nix
 # boostrap RCM
