030c117c9664c1b28cec3a2513448fbbcb90dfed
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-07-31 11:11:50 -0400

    update

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 sudo /nix/var/nix/profiles/default/bin/nix build
-export PATH="$(pwd)/result/bin:$PATH"
+bin_result="$(pwd)/result/bin"
+export PATH="$bin_result:$PATH"
 cp rcrc ~/.rcrc
 printf "DOTFILES_DIRS=\"%s\"" "$(pwd)" >> ~/.rcrc
 ./result/bin/rcup
+fish -c "fish_add_path $bin_result"
 env
 pwd
 # TODO bootstrap nix
