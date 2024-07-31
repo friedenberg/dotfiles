2acabf4302657fbb29e94cea1610a046e488a602
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-07-31 11:08:46 -0400

    update

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 sudo /nix/var/nix/profiles/default/bin/nix build
+export PATH="$(pwd)/result/bin:$PATH"
 cp rcrc ~/.rcrc
-printf "DOTFILES_DIRS=\"%s\"" "$(pwd)"
+printf "DOTFILES_DIRS=\"%s\"" "$(pwd)" >> ~/.rcrc
 ./result/bin/rcup
 env
 pwd
