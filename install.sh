64c19a67bfa46098d9a286f8d7e3e776ebf1bf4b
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-07-31 11:04:11 -0400

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+sudo /nix/var/nix/profiles/default/bin/nix build
+cp rcrc ~/.rcrc
+pwd >> ~/.rcrc
+./result/bin/rcup
 env
 pwd
 # TODO bootstrap nix
