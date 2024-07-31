422ed8c3a1f742620b70b28b01562528f889763f
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-07-31 10:55:47 -0400

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-!# /bin/bash -e
+#! /bin/bash -e
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 env
 # TODO bootstrap nix
 # build nix
