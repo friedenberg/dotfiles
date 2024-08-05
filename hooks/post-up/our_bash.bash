0df4dc64f1a57c81ab1271885fb07b3a9c2c4724
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-08-05 11:08:47 -0400

    update

--- hooks/post-up/our_bash.bash
+++ hooks/post-up/our_bash.bash
@@ -1,6 +1,6 @@
 #! /bin/bash -xe
 
-our_bash="$(readlink ~/.result/bin/bash)"
+our_bash="$(readlink ~/.dotfiles/result/bin/bash)"
 mkdir -p ~/.config/direnv/
 cat - > ~/.config/direnv/direnv.toml <<-EOM
 bash_path = "$our_bash"
