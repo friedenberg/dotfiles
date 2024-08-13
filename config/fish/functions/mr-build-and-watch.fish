9e48760c7f4372bc58c547d0da8d9e2436c4812c
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-08-13 14:51:44 -0400

    update

--- config/fish/functions/mr-build-and-watch.fish
+++
@@ -1,5 +0,0 @@
-function mr-build-and-watch
-  set -l file $argv[1]
-  set -l format $argv[2]
-  fswatch -o0 $file | xargs -0 -L1 bash -c "echo $file changed && zit format-zettel -mode blob $format $file | timg --center -"
-end
