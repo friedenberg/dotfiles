b5556956a50dafd1977c723175e2832defb2ef29
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-10-24 19:57:04 -0400

    update

--- config/fish/0-ssh-config.fish
+++ config/fish/0-ssh-config.fish
@@ -3,6 +3,7 @@ set -l SSH_OLD_AUTH_SOCK $SSH_AUTH_SOCK
 
 if not test -L $HOME/.local/state/ssh/ssh_auth_sock_ssh
   eval (ssh-agent -c) >/dev/null
+  mkdir -p ~/.local/state/ssh
   ln -s $SSH_AUTH_SOCK $HOME/.local/state/ssh/ssh_auth_sock_ssh
   set -e SSH_AUTH_SOCK
 end
