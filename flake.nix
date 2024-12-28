b63057d2175e9d678c1fa1e68a19af9038a1c556
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-12-28 18:27:56 -0500

    update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/0.1.21.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411.712007.tar.gz";
     nixpkgs-stable.url = "nixpkgs/release-24.11";
     utils.url = "github:numtide/flake-utils";
 
@@ -28,6 +29,14 @@
       };
     };
 
+    nix = {
+      url = "github:friedenberg/dev-flake-templates?dir=nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        utils.follows = "utils";
+      };
+    };
+
     # kmonad = {
     #   url = "git+https://github.com/kmonad/kmonad?submodules=1&dir=nix";
     #   inputs.nixpkgs.follows = "nixpkgs";
@@ -45,115 +54,121 @@
     #     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-stable,
-    utils,
-    zit,
-    chrest,
-    chromium-html-to-pdf,
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , nixpkgs-stable
+    , utils
+    , zit
+    , chrest
+    , chromium-html-to-pdf
+    , fh
+    , nix
+    }:
     (utils.lib.eachDefaultSystem
       (system:
-        let
+      let
 
-          pkgs = import nixpkgs {
-            inherit system;
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      rec {
+        packages = {
+          all = pkgs.symlinkJoin {
+            name = "all";
+            paths = with pkgs; [
+              age
+              asdf
+              asdf-vm
+              bashInteractive
+              bats
+              chrest.packages.${system}.default
+              chromium-html-to-pdf.packages.${system}.html-to-pdf
+              # cdparanoia
+              coreutils
+              # csvkit
+              curl
+              curlftpfs
+              dash
+              ddrescue
+              direnv
+              ffmpeg
+              fh.packages.${system}.default
+              figlet
+              fish
+              fontconfig
+              fswatch
+              gawk
+              gftp
+              git
+              git-secret
+              gnumake
+              gnuplot
+              gnupg
+              gpgme
+              graphviz
+              hostess
+              httpie
+              hub
+              imagemagick
+              isolyzer
+              jq
+              just
+              # kmonad
+              # keyd
+              lftp
+              libcdio
+              moreutils
+              neovim
+              nix.packages.${system}.default
+              nix-direnv
+              nixpkgs-fmt
+              ocrmypdf
+              openssh
+              pandoc
+              paperkey
+              # pinentry
+              # pinentry-mac
+              parallel
+              plantuml
+              rcm
+              # reattach-to-user-namespace
+              rsync
+              shellcheck
+              shfmt
+              silver-searcher
+              socat
+              sshpass
+              sshfs-fuse
+              thefuck
+              timidity
+              timg
+              tldr
+              tmux
+              tree
+              uv
+              vim
+              watchexec
+              websocat
+              wget
+              yubico-piv-tool
+              yt-dlp
+              zstd
+              # kmonad.packages.${system}.default
+              # zit.packages.${system}.default
+            ];
           };
 
-        in
-        rec {
-          packages = {
-            all = pkgs.symlinkJoin {
-              name = "all";
-              paths = with pkgs; [
-                  age
-                  asdf
-                  asdf-vm
-                  bashInteractive
-                  bats
-                  chrest.packages.${system}.default
-                  chromium-html-to-pdf.packages.${system}.html-to-pdf
-                  # cdparanoia
-                  coreutils
-                  # csvkit
-                  curl
-                  curlftpfs
-                  dash
-                  ddrescue
-                  direnv
-                  ffmpeg
-                  figlet
-                  fish
-                  fontconfig
-                  fswatch
-                  gawk
-                  gftp
-                  git
-                  git-secret
-                  gnumake
-                  gnuplot
-                  gnupg
-                  gpgme
-                  graphviz
-                  hostess
-                  httpie
-                  hub
-                  imagemagick
-                  isolyzer
-                  jq
-                  just
-                  # kmonad
-                  # keyd
-                  lftp
-                  libcdio
-                  moreutils
-                  neovim
-                  nix-direnv
-                  nixpkgs-fmt
-                  ocrmypdf
-                  openssh
-                  pandoc
-                  paperkey
-                  # pinentry
-                  # pinentry-mac
-                  parallel
-                  plantuml
-                  rcm
-                  # reattach-to-user-namespace
-                  rsync
-                  shellcheck
-                  shfmt
-                  silver-searcher
-                  socat
-                  sshpass
-                  sshfs-fuse
-                  thefuck
-                  timidity
-                  timg
-                  tldr
-                  tmux
-                  tree
-                  uv
-                  vim
-                  watchexec
-                  websocat
-                  wget
-                  yubico-piv-tool
-                  yt-dlp
-                  zstd
-                  # kmonad.packages.${system}.default
-                  # zit.packages.${system}.default
-                ];
-            };
+          default = packages.all;
+        };
 
-            default = packages.all;
-          };
-
-          devShells.default = pkgs.mkShell {
-            buildInputs = packages.all.paths;
-          };
-        })
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            nix.devShells.${system}.default
+          ];
+        };
+      })
     );
 }
