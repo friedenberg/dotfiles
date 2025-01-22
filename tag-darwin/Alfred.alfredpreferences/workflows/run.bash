5eb98c3d29a8bf74d3eb81ac22a8370691fb620b
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-01-22 08:09:35 -0500

    update

--- tag-darwin/Alfred.alfredpreferences/workflows/run.bash
+++ tag-darwin/Alfred.alfredpreferences/workflows/run.bash
@@ -2,8 +2,7 @@
 
 PATH="$HOME/eng/zit/go/zit/build:$HOME/.dotfiles/result/bin:$HOME/eng/chrest/go/build:$PATH"
 
-ZIT_DIR="${ZIT_DIR/#\~/$HOME}"
 export EDITOR="$HOME/.local/bin/vim"
 
-pushd "$ZIT_DIR" >/dev/null
+pushd "$HOME/workspace" >/dev/null
 exec "$@"
