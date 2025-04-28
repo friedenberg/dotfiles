08624b36fcbbec7e6535b0b48736dfb9f8f771ff
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-04-28 13:53:32 -0400

    update

--- dev-flake-templates/system-packages-common/flake.nix
+++ dev-flake-templates/system-packages-common/flake.nix
@@ -3,41 +3,6 @@
     fh.url = "https://flakehub.com/f/DeterminateSystems/fh/0.1.22.tar.gz";
     nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411.717296.tar.gz";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102.tar.gz";
-
-    ssh.url = "path:./ssh";
-    bash.url = "path:./bash";
-
-    zit = {
-      url = "github:friedenberg/zit?dir=go/zit";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        utils.follows = "utils";
-      };
-    };
-
-    chrest = {
-      url = "github:friedenberg/chrest?dir=go";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        utils.follows = "utils";
-      };
-    };
-
-    chromium-html-to-pdf = {
-      url = "github:friedenberg/chromium-html-to-pdf";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        utils.follows = "utils";
-      };
-    };
-
-    pa6e = {
-      url = "github:friedenberg/peripage-A6-bluetooth";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        utils.follows = "utils";
-      };
-    };
   };
 
   outputs =
@@ -45,13 +10,7 @@
     , nixpkgs
     , nixpkgs-stable
     , utils
-    , zit
-    , chrest
-    , chromium-html-to-pdf
     , fh
-    , pa6e
-    , ssh
-    , bash
     }:
     (utils.lib.eachDefaultSystem
       (system:
@@ -144,16 +103,6 @@
               yt-dlp
               # yubikey-manager
               zstd
-            ] ++ [
-              # pa6e-markdown-to-html
-              # html-to-pdf
-              chrest.packages.${system}.default
-              fh.packages.${system}.default
-              chromium-html-to-pdf.packages.${system}.html-to-pdf
-              pa6e.packages.${system}.pa6e-markdown-to-html
-              zit.packages.${system}.default
-              ssh.packages.${system}.default
-              bash.packages.${system}.default
             ];
           };
         };
